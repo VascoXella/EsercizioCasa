9fa034aec986e9e51997b332e17f86bdc9cecf39
Utilizzatore <Utilizzatore@DESKTOP-4DDKKF3>
2024-01-04 16:24:38 +0100

    Aggiungere i file di progetto.

---
+++ EsercizioCasa/Program.cs
@@ -0,0 +1,35 @@
+﻿using EsercizioCasa.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioCasa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float xr = 0;
+            float yr = 0;
+
+            Vector v1 = new Vector(2, 2);
+
+            Vector v2 = new Vector(3, 3);
+
+            // Vector v3 = new Vector();
+            // v3 = Vector.Parse("4; 4");
+
+            v1.Sommav(v2, out xr, out yr);
+
+            Console.WriteLine("{0}, {1}", xr, yr);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EsercizioCasa/Properties/Vector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioCasa.Properties
+{
+    internal class Vector
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+
+        public Vector()
+        {
+        }
+
+        public Vector(float X, float Y)
+        {
+            x = X;
+            y = Y;            
+        }
+
+
+
+        public void Sommav(Vector v2, out float xr, out float yr)
+        {
+            xr = this.x + v2.x;
+            yr = this.y + v2.y;
+            return;
+        }
+
+    }
+}
